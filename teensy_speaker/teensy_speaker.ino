828f5fb998bba7858b5e8f36e6180f427f204cf7
wanganran <wanganran01@gmail.com>
2021-03-03 12:20:51 -0800

    Update teensy_speaker.ino

--- teensy_speaker/teensy_speaker.ino
+++ teensy_speaker/teensy_speaker.ino
@@ -4,7 +4,9 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-#include "chirp.h"
+#include "AudioSampleTeensy.h"
+
+const int PIN_INT=33;
 
 AudioPlayMemory play1;
 AudioPlayMemory play2;
@@ -16,28 +18,46 @@ AudioConnection          patchCord2(play2, 0, i2s_quad, 3);
 AudioConnection          patchCord3(play3, 0, i2s_quad, 0);
 AudioConnection          patchCord4(play4, 0, i2s_quad, 1);
 
+elapsedMicros micro;
+long unsigned int last_clk;
+long unsigned int extclk_cnt;
+ 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   AudioMemory(10);
 
+  //layout:
+  //2 3
+  //1 4
 
   delay(1000);
+  play2.play(AudioSampleTeensy, 0);
+  play3.play(AudioSampleTeensy, 250);
+  play1.play(AudioSampleTeensy, 500);
+  play4.play(AudioSampleTeensy, 750);
   
-  play1.play(AudioSampleTeensy, 0);
-  delay(1000);
-  
-  play2.play(AudioSampleTeensy, 410);
-  
-  delay(1000);
-  play3.play(AudioSampleTeensy, 819);
-  
-  delay(1000);
-  play4.play(AudioSampleTeensy, 1229);
+  pinMode(PIN_INT, INPUT);
+  micro=0;
+  last_clk=0;
+  extclk_cnt=0;
+  attachInterrupt(PIN_INT, extclk, FALLING);
+
+  delay(10);
+}
+
+void extclk(){
+  extclk_cnt++;
+  if(extclk_cnt==10){ // 50k clock = 1s
+    uint32_t m=(uint32_t)micro;
+    micro=0;
+    last_clk=m;
+    extclk_cnt=0;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print(AUDIO_BLOCK_SAMPLES);
+  Serial.println(last_clk);
   delay(1000);
 }
